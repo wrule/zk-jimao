19d45914bac85ef849253b6ad233e62c28e64fad
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-21 16:42:51 +0800

    prj_id

--- truffle-config.js
+++ truffle-config.js
@@ -85,7 +85,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
     goerli: {
-      provider: () => new HDWalletProvider(secret.pri_key, `https://goerli.infura.io/v3/${PROJECT_ID}`),
+      provider: () => new HDWalletProvider(secret.pri_key, `https://goerli.infura.io/v3/${secret.prj_id}`),
       network_id: 5,       // Goerli's id
       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
