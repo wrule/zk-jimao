9b9a2bee3e2536d6bf979fd1eb1c2ba758801fde
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-21 16:37:13 +0800

    提交代码

--- truffle-config.js
+++ truffle-config.js
@@ -46,6 +46,8 @@
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
+const secret = require('./.secret.json');
+
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -83,7 +85,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
     goerli: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
+      provider: () => new HDWalletProvider(secret.pri_key, `https://goerli.infura.io/v3/${PROJECT_ID}`),
       network_id: 5,       // Goerli's id
       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
